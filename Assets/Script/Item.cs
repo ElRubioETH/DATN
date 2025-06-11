12893a3ca3bccb0974d099a9cc9dcfe495b9d898
ElRubioETH <henkenmanybruh@gmail.com>
2025-06-11 18:02:21 +0700

    hehe

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -1,30 +1,5 @@
 ﻿using UnityEngine;
 
-[System.Serializable]
-public class Item
-{
-    public string itemName;
-    public int quantity;
-    public ItemType itemType;
-    public int value;
-
-    public Item(string name, int qty, ItemType type, int val)
-    {
-        itemName = name;
-        quantity = qty;
-        itemType = type;
-        value = val;
-    }
-}
-
-public enum ItemType
-{
-    Weapon,
-    Potion,
-    Resource,
-    Food,
-    Other
-}
 
 public class ItemWorld : MonoBehaviour
 {
